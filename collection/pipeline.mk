6bb8e872e9bb5b5a3e1bfeed2dcf8e953fec0a0a
Jon Bramley <brammers@gmail.com>
2021-01-14 10:35:54 +0000

    fix: remove old version of bucks data

--- collection/pipeline.mk
+++ collection/pipeline.mk
@@ -1,10 +1,6 @@
 DEVELOPMENT_POLICY_DATASET=$(DATASET_DIR)development-policy.csv
 DEVELOPMENT_POLICY_TRANSFORMED_FILES=\
-    $(TRANSFORMED_DIR)development-policy/3dc2158d76387bd9d4434418a22dbca9c3066362f7e67612da1d42f97e1045d5.csv\
     $(TRANSFORMED_DIR)development-policy/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv
-
-$(TRANSFORMED_DIR)development-policy/3dc2158d76387bd9d4434418a22dbca9c3066362f7e67612da1d42f97e1045d5.csv: collection/resource/3dc2158d76387bd9d4434418a22dbca9c3066362f7e67612da1d42f97e1045d5
-	$(run-pipeline)
 
 $(TRANSFORMED_DIR)development-policy/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv: collection/resource/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a
 	$(run-pipeline)
