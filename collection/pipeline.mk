a7e9877d8e0e57eb24f4bb4e86392a89d4f7e311
Jon Bramley <brammers@gmail.com>
2021-05-11 11:43:15 +0100

    fix: resource had duplicate category

--- collection/pipeline.mk
+++ collection/pipeline.mk
@@ -2,7 +2,7 @@ DEVELOPMENT_POLICY_DATASET=$(DATASET_DIR)development-policy.csv
 DEVELOPMENT_POLICY_TRANSFORMED_FILES=\
     $(TRANSFORMED_DIR)development-policy/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv
 
-$(TRANSFORMED_DIR)development-policy/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv: collection/resource/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a
+$(TRANSFORMED_DIR)development-policy/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv: fixed/781ef38ab8a51f6a4a0e603d8ddf8c65a5c4f9e707db3fa25e31f0614b3eae9a.csv
 	$(run-pipeline)
 
 $(DEVELOPMENT_POLICY_DATASET): $(DEVELOPMENT_POLICY_TRANSFORMED_FILES)
